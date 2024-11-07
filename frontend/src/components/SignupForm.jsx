51024f255f0c985b2b8833256bbb706518875378
LennyYiWANG <n11401184@qut.edu.au>
2024-11-07 15:48:05 +1100

    Create login and registration components, use route navigation to navigate between different pages, and build the basic framework for login and registration functionality.

---
+++ frontend/src/components/SignupForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const SignUpForm =()=>{
+
+    return(
+        <div>
+            SignUpForm
+        </div>
+    )
+}
+
+export default SignUpForm
